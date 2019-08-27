7a9ba60e300fac82f31e9e00415b4e84122e3192
anskydev <dev@ansky.sh>
2019-08-27 09:37:57 -0600

    create SEO component

---
+++ src/components/SEO/SEO.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import { Helmet } from 'react-helmet'
+import { string } from 'prop-types'
+
+const SEO = ({ title, description }) => {
+  const { site: { siteMetadata } } = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+  `)
+
+  const fullTitle = title ? `${title} | ${siteMetadata.title}` : siteMetadata.title
+
+  return (
+    <Helmet>
+      <title>{fullTitle}</title>
+      <meta name="title" content={fullTitle} />
+      <meta name="description" content={description || siteMetadata.description} />
+    </Helmet>
+  )
+}
+
+SEO.propTypes = {
+  title: string.isRequired,
+  description: string.isRequired
+}
+
+export default SEO
