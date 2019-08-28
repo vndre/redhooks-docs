3d177c9bd8dd222c590d321c4f82a2a6f196bf1e
anskydev <dev@ansky.sh>
2019-08-28 11:51:22 -0600

    update Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import { element } from 'prop-types'
+import { Styled } from 'theme-ui'
+import { Global } from '@emotion/core'
+import Hero from '../Hero/Hero'
 
 const Layout = ({ children }) => (
-  <div>
-    <h1>header</h1>
-    {children}
-  </div>
+  <Styled.root>
+    <Global
+      styles={(theme) => ({
+        body: {
+          color: theme.colors.text,
+          margin: 0,
+          backgroundColor: theme.colors.background
+        },
+        '*': {
+          boxSizing: 'border-box'
+        }
+      })}
+    />
+    <Hero />
+    <article>
+      {children}
+    </article>
+  </Styled.root>
 )
 
 Layout.propTypes = {
