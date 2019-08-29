cabddea34139d541ece1356a6f91f2f2e05e816b
anskydev <dev@ansky.sh>
2019-08-29 13:17:28 -0600

    update Hero layout

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,20 +1,57 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 import {
-  Flex, Heading, Text, Box
+  Flex, Heading, Text, Box, Link
 } from 'rebass'
 import { rem } from 'polished'
 import { ReactComponent as ArrowIcon } from '../../assets/svg/arrow-down.svg'
+import { ReactComponent as RepoIcon } from '../../assets/svg/repository.svg'
 
-const Hero = () => (
+const Hero = (props) => {
+  const { site } = useStaticQuery(graphql`
+    query repoURL {
+      site {
+        siteMetadata {
+          repository
+        }
+      }
+    }
+  `)
+
+  return (
     <Flex
       as='section'
-      p={[4, 5, 6]}
+      p={[4, 5]}
       height='100vh'
       justifyContent='center'
       alignItems='center'
       flexDirection='column'
       maxWidth={rem('1500px')}
+      css={{
+        position: 'relative'
+      }}
+      {...props}
     >
+      <Box
+        css={{
+          position: 'absolute',
+          top: rem('10px'),
+          right: rem('10px')
+        }}
+      >
+        <Link
+          href={site.siteMetadata.repository}
+          color='secondary'
+          alt='repository'
+        >
+          <RepoIcon
+            css={{
+              width: rem('32px'),
+              height: rem('32px')
+            }}
+          />
+        </Link>
+      </Box>
       <Heading
         fontSize={[5, 6]}
         color='primary'
@@ -46,7 +83,7 @@ const Hero = () => (
         css={{
           position: 'absolute',
           bottom: rem('20px'),
-          cursor: 'pointer',
+          userSelect: 'none',
 
           '&:hover': {
             svg: {
@@ -70,6 +107,7 @@ const Hero = () => (
         />
       </Box>
     </Flex>
-)
+  )
+}
 
 export default Hero
