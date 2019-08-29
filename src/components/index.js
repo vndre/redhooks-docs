432ca5d4e2242de45deaae175f064f95752fd723
anskydev <dev@ansky.sh>
2019-08-28 19:54:46 -0600

    update components folder index

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
-import Doc from './Doc/Doc'
-
-export { Doc }
+export { default as Doc } from './Doc/Doc'
+export { default as NavItems } from './NavItems/NavItems'
+export { default as Global } from './Global/Global'
+export { default as Hero } from './Hero/Hero'
