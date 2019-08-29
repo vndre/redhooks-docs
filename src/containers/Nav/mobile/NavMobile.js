5ffac4e3e47f30f2108622a32ad49d043cf6e68a
anskydev <dev@ansky.sh>
2019-08-29 13:19:57 -0600

    update NavMobile layout and styles

--- src/containers/Nav/mobile/NavMobile.js
+++ src/containers/Nav/mobile/NavMobile.js
@@ -1,69 +1,101 @@
-import React, { useState } from 'react'
-import { Flex, Box, Button } from 'rebass'
-import { arrayOf, object } from 'prop-types'
+import React, { useEffect, useRef } from 'react'
+import { Box, Button } from 'rebass'
+import {
+  arrayOf, object, bool, func
+} from 'prop-types'
+import anime from 'animejs/lib/anime.min'
+import { rem, cover } from 'polished'
 import { ReactComponent as MenuIcon } from '../../../assets/svg/bookmark.svg'
-import { ReactComponent as GitIcon } from '../../../assets/svg/git-fork.svg'
 import { NavItems } from '../../../components'
+import theme from '../../../theme'
 
-const NavMobile = ({ navItems }) => {
-  // eslint-disable-next-line
-  const [open, setOpen] = useState(false)
+const NavMobile = ({ navItems, open, setOpen }) => {
+  const menuEl = useRef(null)
+  const overlayEl = useRef(null)
+
+  useEffect(() => {
+    anime({
+      targets: menuEl.current,
+      translateX: open ? '0%' : '-110%',
+      duration: 400,
+      easing: 'easeInCubic'
+    })
+
+    const overlayTL = anime.timeline({
+      targets: overlayEl.current
+    })
+
+    overlayTL
+      .add({
+        translateX: open ? '0%' : '-100%',
+        duration: 50
+      })
+      .add({
+        opacity: open ? '0.1' : '0',
+        duration: 500
+      })
+  }, [open])
+
+  const itemClickAction = () => {
+    setOpen()
+    document.getElementById('content').scrollIntoView()
+  }
 
   return (
     <>
-      <Flex
-        as='header'
-        alignItems='center'
-      >
-        <Button
-          sx={{
-            pl: 0,
-            p: 2
-          }}
-        >
-          <MenuIcon
-            css={{
-              fill: '#080808'
-            }}
-          />
-        </Button>
-        <Button
-          variant='primary'
-          pr={0}
-          p={2}
-          ml='auto'
-          bg='transparent'
-          sx={{
-            display: 'flex',
-            border: ({ colors }) => `1px solid ${colors.text}`
-          }}
-        >
-          <GitIcon
-            css={{
-              fill: '#080808'
-            }}
-          />
-        </Button>
-      </Flex>
-      <Box
-        as='nav'
-        bg='background'
+      <Button
+        variant='primary'
+        pl={0}
+        p={2}
+        bg='transparent'
         sx={{
-          position: 'absolute',
-          top: 0,
-          bottom: 0,
-          left: 0,
-          boxShadow: ({ shadows }) => shadows.large
+          display: 'flex'
+          // border: ({ colors }) => `1px solid ${colors.text}`
+        }}
+        onClick={setOpen}
+      >
+        <MenuIcon
+          css={{
+            fill: theme.colors.text,
+            width: rem('32px'),
+            height: rem('32px')
+          }}
+        />
+      </Button>
+      <div
+        ref={overlayEl}
+        onClick={setOpen}
+        css={{
+          ...cover(),
+          background: theme.colors.text
         }}
       >
-        <NavItems items={navItems} />
+      </div>
+      <Box
+        ref={menuEl}
+        as='nav'
+        bg='text'
+        sx={{
+          position: 'absolute',
+          transform: 'translateY(-110%)',
+          top: 0,
+          bottom: 0,
+          left: 0
+        }}
+      >
+        <NavItems
+          items={navItems}
+          clickAction={itemClickAction}
+        />
       </Box>
     </>
   )
 }
 
 NavMobile.propTypes = {
-  navItems: arrayOf(object).isRequired
+  navItems: arrayOf(object).isRequired,
+  open: bool.isRequired,
+  setOpen: func.isRequired
 }
 
 export default NavMobile
